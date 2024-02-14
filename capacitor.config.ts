9c0688926cb5d45443d0ef98877443ff2837015e
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-14 20:05:01 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.CampusConnect.app',
+  appName: 'CampusConnect',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
