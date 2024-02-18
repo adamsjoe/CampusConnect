3908f114aa43b8eae1abf7400c4e6e4bae58b287
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-18 16:17:24 +0000

    Adding Styling. Also adding in data from the datastore

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { firebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ProfilePage {
+  aboutInfo: any;
+  constructor(private firbaseService: firebaseService) {
+    firbaseService.getAboutInfo().subscribe((data) => {
+      this.aboutInfo = data;
+    });
   }
-
 }
