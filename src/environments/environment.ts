e8be41d8cc245a9cb41f95f96a669dae1bfc3064
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-18 16:17:40 +0000

    Adding firebase creds - the ksys SHOULD NOT BE HERE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  // firebase = {}
+  firebase: {
+    apiKey: 'AIzaSyDoVibJjdwpQur8mjMzhWKujOAvDBVUJIs',
+    authDomain: 'mad-temp.firebaseapp.com',
+    projectId: 'mad-temp',
+    storageBucket: 'mad-temp.appspot.com',
+    messagingSenderId: '1085569108008',
+    appId: '1:1085569108008:web:a75587317cafd894c84d1d',
+    measurementId: 'G-5GBV20RJ4L',
+  },
 };
 
 /*
