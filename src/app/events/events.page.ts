bf05a16b6075496b9bc31df0839c2dc973421669
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-18 19:32:32 +0000

    Adding some dates in to show progress

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './events.page.html',
   styleUrls: ['./events.page.scss'],
 })
-export class EventsPage implements OnInit {
+export class EventsPage {
+  highlightedDates = [
+    {
+      date: '2023-01-05',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2023-01-10',
+      textColor: '#09721b',
+      backgroundColor: '#c8e5d0',
+    },
+    {
+      date: '2023-01-20',
+      textColor: 'var(--ion-color-secondary-contrast)',
+      backgroundColor: 'var(--ion-color-secondary)',
+    },
+    {
+      date: '2023-01-23',
+      textColor: 'rgb(68, 10, 184)',
+      backgroundColor: 'rgb(211, 200, 229)',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
