199e7614fe15cf9c1908b5c0db10d458f60f3775
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-18 19:14:16 +0000

    Updates to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDoVibJjdwpQur8mjMzhWKujOAvDBVUJIs',
+    authDomain: 'mad-temp.firebaseapp.com',
+    projectId: 'mad-temp',
+    storageBucket: 'mad-temp.appspot.com',
+    messagingSenderId: '1085569108008',
+    appId: '1:1085569108008:web:a75587317cafd894c84d1d',
+    measurementId: 'G-5GBV20RJ4L',
+  },
 };
