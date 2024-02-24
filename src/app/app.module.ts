ed63a56e3248c5aaf736a12f670fd27a52dd3ed3
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-24 18:26:19 +0000

    Imported FormsModule to allow ngModel bindings in the modal to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,15 @@ import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from 'src/environments/environment';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { EventModalComponent } from './event-modal/event-modal.component';
+import { NewPostModalComponent } from './new-post-modal/new-post-modal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, EventModalComponent],
+  declarations: [AppComponent, EventModalComponent, NewPostModalComponent],
   imports: [
     BrowserModule,
     IonicModule,
+    FormsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
