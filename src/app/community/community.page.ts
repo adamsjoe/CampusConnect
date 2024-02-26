45de0f550e23c1c9d554483316546922870352d7
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-02-26 21:43:11 +0000

    Adding functions to check if user has joined a group

--- src/app/community/community.page.ts
+++ src/app/community/community.page.ts
@@ -7,13 +7,33 @@ import { UserService } from '../services/currentUser.service';
   templateUrl: './community.page.html',
   styleUrls: ['./community.page.scss'],
 })
-export class CommunityPage {
+export class CommunityPage implements OnInit {
   allGroups: any;
-  constructor(private groups: GroupsService, private userService: UserService) {
-    const user = this.userService.getUser();
+  userJoinedGroups: any[] = []; // Store the groups joined by the user
 
-    groups.getAllGroups().subscribe((data) => {
+  constructor(
+    private groups: GroupsService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    // Fetch all groups
+    this.groups.getAllGroups().subscribe((data) => {
       this.allGroups = data;
+
+      // Fetch groups joined by the user
+      const user = this.userService.getUser();
+      if (user) {
+        this.groups.getUserJoinedGroups(user).subscribe((joinedGroups) => {
+          // Store the groups joined by the user
+          this.userJoinedGroups = joinedGroups.map((group) => group.groupId);
+        });
+      }
     });
+  }
+
+  // Check if the user has joined the group
+  isUserJoinedGroup(groupId: string): boolean {
+    return this.userJoinedGroups.includes(groupId);
   }
 }
--- src/app/services/groups.service.ts
+++ src/app/services/groups.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  query,
+  where,
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +14,19 @@ import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 export class GroupsService {
   constructor(private firestore: Firestore) {}
 
+  // return all the groups, key this in the document id
   getAllGroups(): Observable<any> {
     const allGroupsCollection = collection(this.firestore, 'groups');
-    return collectionData(allGroupsCollection);
+    return collectionData(allGroupsCollection, { idField: 'id' });
+  }
+
+  // query the joinedGroups collection, filtering on the
+  // userId
+  getUserJoinedGroups(userId: string): Observable<any[]> {
+    const userJoinedGroupsQuery = query(
+      collection(this.firestore, 'joinedGroups'),
+      where('userId', '==', userId)
+    );
+    return collectionData(userJoinedGroupsQuery);
   }
 }
